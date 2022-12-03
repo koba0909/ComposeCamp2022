5cf6acaf7b8203f00db21a73880b787dbaaee549
koba <koba1mobile@gmail.com>
2022-12-03 15:06:51 +0900

    [pathway1] modifier를 활요하여 vertical, horizontal padding 추가

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -30,7 +31,7 @@ private fun MyApp(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
         }
@@ -39,15 +40,21 @@ private fun MyApp(
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Column(modifier = Modifier.padding(24.dp)) {
+    Surface(
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        color = MaterialTheme.colors.primary
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(24.dp)
+        ) {
             Text(text = "Hello, ")
             Text(text = name)
         }
     }
 }
 
-@Preview(showBackground = true, name = "Text Preview")
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
